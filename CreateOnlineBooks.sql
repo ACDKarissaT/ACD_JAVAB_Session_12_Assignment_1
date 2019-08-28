db8d2bad3f09a8f08ac983de02ee49b448c790a2
karituason <karituason@gmail.com>
2019-08-27 23:40:54 -0400

    Added database creation sql

---
+++ CreateOnlineBooks.sql
@@ -0,0 +1,11 @@
+create database OnlineBooks;
+use onlinebooks;
+create table books (
+	title varchar(45),
+	authors varchar(45),
+	publisher varchar(45) ,
+    publication_year Date ,
+    price double (8,2),
+    book_id int(11)
+);
+
